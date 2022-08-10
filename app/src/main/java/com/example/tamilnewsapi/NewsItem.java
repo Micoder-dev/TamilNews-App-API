8d07168dd012d39062165bfb4838f32391b27686
Micoder-dev <micoder.com@gmail.com>
2022-08-10 23:26:27 +0530

    CREATING A MODEL

---
+++ app/src/main/java/com/example/tamilnewsapi/NewsItem.java
@@ -0,0 +1,29 @@
+package com.example.tamilnewsapi;
+
+public class NewsItem {
+
+    private String mImageUrl, mTitle, mDescription, mTime;
+
+    public NewsItem(String imageUrl, String title, String description, String time) {
+        mImageUrl = imageUrl;
+        mTitle = title;
+        mDescription = description;
+        mTime = time;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public String getTime() {
+        return mTime;
+    }
+}
